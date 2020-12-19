dc102f3652213539fdd5810ba88a0b0865b63139
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-19 16:26:01 -0500

    added finished navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import WebNavbar from "./components/Navbar";
+
+import HomePage from "./pages/Homepage";
 
 function App() {
   return (
     <div>
-      <WebNavbar />
+      <Router basename="/">
+        <WebNavbar />
+        <Route exact path={["/", "/home"]}>
+          <HomePage />
+        </Route>
+
+      </Router>
     </div>
   );
 }
