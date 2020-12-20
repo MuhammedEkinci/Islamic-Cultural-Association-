0ea22768edf9eb0a175c530518b07c24aa5d1051
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-20 05:30:03 -0500

    fixed navbar bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/Homepage";
 
 function App() {
   return (
     <div>
       <Router basename="/">
+        <Navbar />
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/home" component={HomePage} />
       </Router>
     </div>
   );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
-import Navbar from "../components/Navbar";
+
 
 export default function HomePage() {
     return (
         <div>
-            <Navbar />
             <Jumbotron />
         </div>
     );
