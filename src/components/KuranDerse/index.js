b29da09b4531f69a4060c26adff7a0e8bb7c5b8b
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-20 17:28:21 -0500

    adding activity section component

---
+++ src/components/KuranDerse/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function KuranDerse() {
+    return (
+        <h1>Kuran Derse</h1>
+    );
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
-
+import KuranDerse from "../components/KuranDerse";
 
 export default function HomePage() {
     return (
         <div>
             <Jumbotron />
+            <KuranDerse />
         </div>
     );
 }
