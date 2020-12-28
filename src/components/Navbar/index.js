a2990a1bbcc23780f7274d56499eae499db4cb6c
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-12-28 15:25:34 -0500

    updated navbar brand name

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ import "../../styles/Navbar.css";
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top web-nav container-fluid">
-            <a className="navbar-brand " href="#hero-top">Islamic Cultural Association</a>
+            <a className="navbar-brand " href="#hero-top">Elkton Maryland İslamic Cultural Center</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
